f078018b081bb749b669d4d850c8bf390c5be2db
dev912883 <87614081+dev912883@users.noreply.github.com>
2021-09-30 17:04:51 +0300

    Add files via upload

--- boddea/js/main.js
+++ boddea/js/main.js
@@ -6,7 +6,29 @@ $(document).ready(function(){
         arrows: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        infinite: true
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+		responsive: [
+			{
+			  breakpoint: 1140,
+			  settings: {
+				slidesToShow: 3
+			  }
+			},
+			{
+			  breakpoint: 767,
+			  settings: {
+				slidesToShow: 2
+			  }
+			},
+			{
+				breakpoint: 575,
+				settings: {
+				  slidesToShow: 1
+				}
+			},
+		  ]
     });
 
     // page up
@@ -18,7 +40,7 @@ $(document).ready(function(){
 
     // Menu
     const menu = $('.header__box'),
-    menuItem = $('.header__menu ul li'),
+    menuItem = $('.header__menu ul li:not(.header__lang)'),
     hamburger = $('.burger');
 
     hamburger.on('click', function() {
@@ -26,6 +48,13 @@ $(document).ready(function(){
         menu.toggleClass('active');
         $('body').toggleClass('overlay-menu');
         $('.header__logo__mob').toggleClass('hide');
+        if ($('.header').hasClass('overflow-hidden')) {
+			$('.header').toggleClass('overflow-hidden');
+			$('.header').toggleClass('overflow-visible');
+        } else {
+			$('.header').toggleClass('overflow-visible');
+			$('.header').toggleClass('overflow-hidden');
+		}
     });
 
     menuItem.on('click', function() {
@@ -33,16 +62,16 @@ $(document).ready(function(){
         menu.toggleClass('active');
         if ($('body').hasClass('overlay-menu')) {
           $('body').toggleClass('overlay-menu');
-        }
-        else {
+        } else {
           return;
         }
         if ($('.header__logo__mob').hasClass('hide')) {
             $('.header__logo__mob').toggleClass('hide');
-          }
-          else {
+          } else {
             return;
-          }
+         }
+		 $('.header').toggleClass('overflow-visible');
+		 $('.header').toggleClass('overflow-hidden');
     });
 
 });
