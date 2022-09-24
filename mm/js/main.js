8146b9c3f011bb20410756003c8aa6d6afd6b001
dev912883 <87614081+dev912883@users.noreply.github.com>
2022-09-24 13:24:56 +0300

    Add files via upload

--- mm/js/main.js
+++ mm/js/main.js
@@ -58,35 +58,4 @@ $(document).ready(function(){
 	$('.modal__close').on('click', function() {
 		$('.overlay, #thanks, #call, #question').fadeOut();
 	});
-
-    // Send form
-    // $('form').submit(function(e) {
-    //     e.preventDefault();
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "mailer/smart.php",
-    //         data: $(this).serialize()
-    //     }).done(function() {
-    //         $(this).find("input").val("");
-    //         // $('#consult, #order').fadeOut();
-    //         $('.overlay, #thanks').fadeIn();
-    //         $('form').trigger('reset');
-    //     });
-    //     return false;
-    // });
-
-    // Smooth scroll and page up
-    // $(window).scroll(function() {
-    //     if ($(this).scrollTop() > 1600) {
-    //         $('.pageup').fadeIn();
-    //     } else {
-    //         $('.pageup').fadeOut();
-    //     }
-    // });
-
-    // $("a[href^='#']").click(function(){
-    //     const _href = $(this).attr("href");
-    //     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-    //     return false;
-    // });
 });
